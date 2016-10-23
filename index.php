ae227430310b3b621ed13212015cfbb522471c5c
marciopelegrini <marcio.pelegrini@gmail.com>
2016-10-23 10:02:54 -0400

    Test with Deploy

--- index.php
+++ index.php
@@ -10,9 +10,10 @@
         echo "Funciona abestado! <br/>";
         echo "Teste 2 ";
         ?>
-        <div style="background-color: menu; border: 1px solid red;">
+        <div style="background-color: menu; border: 1px solid blue;">
             <b>Aqui &eacute; outra DIV</b><br/>
 	    <b>Texto qualquer de novo</b>
+	    <b>Teste com GIT deploy diretamente do Git<b>
         </div>
     </body>
 </html>
